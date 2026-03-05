78fb9f21deb030aa298b3db5f166838c31b4eee5
juanjosezapata0507 <juanjosezapatavillada10@gmail.com>
2026-03-04 21:52:26 -0500

    implementación endpoint POST y GET /reservas

--- Reserva.py
+++ Reserva.py
@@ -15,3 +15,23 @@ class Reserva(BaseModel):
     estado: str
 
 reservas = []
+
+class Reserva(BaseModel):
+    id_reserva: int
+    id_sala: int
+    id_usuario: int
+    fecha: str
+    hora_inicio: str
+    hora_fin: str
+    personas: int
+    estado: str
+
+    @app.post("/reservas")
+def crear_reserva(reserva: Reserva):
+    reservas.append(reserva)
+    return {"mensaje": "Reserva creada correctamente"}
+
+    @app.get("/reservas")
+def listar_reservas():
+    return reservas
+    
