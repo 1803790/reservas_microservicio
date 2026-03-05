b8703dc8b554e3129587bec56a41689cdaa2bf9b
Santiago Orrego <orregosantiago72@gmail.com>
2026-03-04 19:44:04 -0500

    Aporte: Modelo de datos de Reservas con pydantic

---
+++ Reserva.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Reserva(BaseModel):
+    id_reserva: int
+    id_sala: int
+    id_usuario: int
+    fecha: str
+    hora_inicio: str
+    hora_fin: str
+    personas: int
+    estado: str
+
+reservas = []
